ff7cae9fa21afe98bdf02ebfe1472dc5ed8176de
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2019-08-12 11:20:17 +0200

    Start without additional permissions

--- server.js
+++ server.js
@@ -2,7 +2,22 @@
 const prerender = require('prerender');
 
 var server = prerender({
-    chromeLocation: '/usr/bin/chromium-browser'
+    chromeLocation: '/usr/bin/chromium-browser',
+    chromeFlags: [
+        "/usr/bin/chromium",
+        "--no-sandbox",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--disable-default-apps",
+        "--disable-popup-blocking",
+        "--disable-translate",
+        "--disable-background-timer-throttling",
+        "--headless",
+        "--disable-gpu",
+        "--remote-debugging-address=0.0.0.0",
+        "--remote-debugging-port=9222",
+        "--hide-scrollbars"
+      ]
 });
 // server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
